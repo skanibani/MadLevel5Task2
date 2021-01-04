f4431b9de97867dadbd33ac715f2854c6a65b9ac
Skander <skander@bibani.nl>
2021-01-04 11:30:42 +0100

    Add AddGameFragment layout and functionality

--- app/src/main/java/com/example/madlevel5task2/ui/AddGameFragment.kt
+++ app/src/main/java/com/example/madlevel5task2/ui/AddGameFragment.kt
@@ -5,22 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.madlevel5task2.R
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
+import com.example.madlevel5task2.databinding.FragmentAddGameBinding
+import com.example.madlevel5task2.model.Game
+import java.util.*
+
 class AddGameFragment : Fragment() {
+
+    private var _binding: FragmentAddGameBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: GameViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_game, container, false)
+        _binding = FragmentAddGameBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.fabAddGame.setOnClickListener {
+            addGame()
+            findNavController().navigate(R.id.action_AddGameFragment_to_GameBacklogFragment)
+        }
+    }
+
+    private fun addGame() {
+        val newGame = Game(
+            binding.inputGameTitle.text.toString(),
+            binding.inputGamePlatform.text.toString(),
+            Calendar.getInstance().time
+        )
+
+        viewModel.insertGame(newGame)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/madlevel5task2/ui/GameBacklogFragment.kt
+++ app/src/main/java/com/example/madlevel5task2/ui/GameBacklogFragment.kt
@@ -1,12 +1,15 @@
 package com.example.madlevel5task2.ui
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel5task2.R
@@ -14,13 +17,14 @@ import com.example.madlevel5task2.R
 import com.example.madlevel5task2.databinding.FragmentGameBacklogBinding
 import com.example.madlevel5task2.model.Game
 import java.util.*
+import kotlin.collections.ArrayList
 
 class GameBacklogFragment : Fragment() {
 
     private var _binding: FragmentGameBacklogBinding? = null
     private val binding get() = _binding!!
 
-    private val backlogGames = arrayListOf<Game>()
+    private val backlogGames = mutableListOf<Game>()
     private val gameAdapter = GameAdapter(backlogGames)
 
     private lateinit var viewModel: GameViewModel
@@ -49,10 +53,16 @@ class GameBacklogFragment : Fragment() {
     private fun initViews() {
         binding.rvGames.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         binding.rvGames.adapter = gameAdapter
+
+        // Fab
+        binding.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_GameBacklogFragment_to_AddGameFragment)
+        }
     }
 
     private fun observeViewModel() {
         viewModel.games.observe(viewLifecycleOwner, Observer<List<Game>> {
+            backlogGames.clear()
             backlogGames.addAll(it)
             gameAdapter.notifyDataSetChanged()
         })
